dcccad7475a9cda75ee5d788052838f10c7f951d
Paolo Mottadelli <paolo.moz@gmail.com>
2024-04-12 09:23:38 +0200

    homesearch form

--- blocks/carousel/carousel.js
+++ blocks/carousel/carousel.js
@@ -1,5 +1,38 @@
 import { fetchPlaceholders } from '../../scripts/aem.js';
 
+function buildSearchForm() {
+  const sfHtml = `<div>
+    <div class='row'><img src='/images/searchbymap.png'/></div>
+    <div class='row'><input class='homesearch form-control' type='text' placeholder='Type a Community, Plan Name, Address, Metro Area, City, MLS Number'/></div>
+    <div class='row'>
+      <label for="HomeStyle" class="sr-only">Home Type</label>
+      <select name="HomeStyle" id="HomeStyle" class="homesearchselect homesearch-hometype form-control">
+        <option value="">Home Type</option><option value="1">1 Story</option><option value="6">1.5 Story</option><option value="4">2 Story</option><option value="5">Primary Down</option>
+      </select>
+      <label for="PriceRange" class="sr-only">Price Range</label>
+      <select name="PriceRange" id="PriceRange" class="homesearchselect homesearch-price form-control">
+        <option value="">Price</option><option value="1">$300 - $399</option><option value="2">$400 - $499</option><option value="3">$500 - $599</option>
+      </select>
+      <label for="Beds" class="sr-only">Beds</label>
+      <select name="Beds" id="Beds" class="homesearchselect homesearch-bed  form-control">
+        <option value="">Bed</option><option value="2">2+</option><option value="3">3+</option><option value="4">4+</option><option value="5">5+</option>
+      </select>
+      <label for="City" class="sr-only">City</label>
+      <select name="City" id="City" class="homesearchselect homesearch-city form-control">
+        <option value="">City</option><option value="8">Caldwell</option><option value="10">Kuna</option><option value="4">Meridian</option><option value="9">Middleton</option><option value="5">Nampa</option><option value="6">Star</option>
+      </select>
+      <button type="button" class="btn dropdown-toggle homesearchselect btn-block" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+				More <span class="caret"></span>
+			</button>
+      <button type="submit" name="Submit" value="SEARCH>" class="btn btn-primary homesearchbutton search-submit"><i class="far fa-search"></i> Search Communities</button>  
+    </div>
+  </div>`;
+  const sf = document.createElement('div');
+  sf.classList.add('search-form');
+  sf.innerHTML = sfHtml;
+  return sf;
+}
+
 function updateActiveSlide(slide) {
   const block = slide.closest('.carousel');
   const slideIndex = parseInt(slide.dataset.slideIndex, 10);
@@ -141,6 +174,7 @@ export default async function decorate(block) {
     row.remove();
   });
 
+  container.append(buildSearchForm());
   container.append(slidesWrapper);
   block.prepend(container);
 
