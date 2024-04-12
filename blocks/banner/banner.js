f5f3445b3c4dc7dfb1cd26797bf30f85c84f1212
Paolo Mottadelli <paolo.moz@gmail.com>
2024-04-12 13:00:07 +0200

    cards, yellow section, margins in page

---
+++ blocks/banner/banner.js
@@ -0,0 +1,13 @@
+export default function decorate(block) {
+  const text = block.querySelector('div:nth-child(2)');
+  text.classList.add('spotlightcircle');
+
+  const formHtml = `<div class="input-group">
+    <div class="form-group"><input type="text" name="email" id="email" class="form-control input-sm" required="" placeholder="Enter Email"></div>
+     <span class="input-group-btn"><button type="submit" class="btn btn-primary btn-sm" aria-label="Submit">SUBMIT</button></span>
+  </div>`;
+  const form = document.createElement('div');
+  form.innerHTML = formHtml;
+
+  text.append(form);
+}
